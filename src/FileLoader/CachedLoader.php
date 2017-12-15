b32d4ba784bfe406032cb3f1120814b6c35bdd16
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2017-12-15 16:35:16 +0000

    Add in PSR-16 cached loader

---
+++ src/FileLoader/CachedLoader.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace AuronConsulting\OpenApi\Validation\FileLoader;
+
+use AuronConsulting\OpenApi\Validator\FileLoader\FileLoaderInterface;
+use Psr\SimpleCache\CacheInterface;
+
+/**
+ * This implements a cached file loader, as parsing specs can be rather resource intensive.
+ *
+ * You must provide with a PSR-16 implementation, and one of the actual file loaders.
+ *
+ * @author  Luis Pabon / https://github.com/AuronConsulting
+ * @license MIT
+ */
+class CachedLoader implements FileLoaderInterface
+{
+    /**
+     * @var CacheInterface
+     */
+    private $cache;
+
+    /**
+     * @var FileLoaderInterface
+     */
+    private $loader;
+
+    public function __construct(CacheInterface $cache, FileLoaderInterface $loader)
+    {
+        $this->cache  = $cache;
+        $this->loader = $loader;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function load(string $specLocation): array
+    {
+        $key  = \md5($specLocation);
+        $spec = $this->cache->get($key);
+
+        if ($spec === null) {
+            $spec = $this->loader->load($specLocation);
+            $this->cache->set($key, $spec);
+        }
+
+        return $spec;
+    }
+}
---
+++ src/FileLoader/YamlLoader.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luis
+ * Date: 15/12/17
+ * Time: 16:22
+ */
+
+namespace AuronConsulting\OpenApi\Validation\FileLoader;
+
+class YamlLoader
+{
+
+}
